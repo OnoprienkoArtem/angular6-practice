611c656f9fb55c73493398625f96758f15157220
OnoprienkoArtem <o.artem.e@gmail.com>
2018-06-02 23:25:40 +0300

    work with object date return message

--- task1/src/app/time-module/time-module.component.ts
+++ task1/src/app/time-module/time-module.component.ts
@@ -16,12 +16,22 @@ export class TimeModuleComponent implements OnInit {
   month = this.date.getMonth();
   monthList: string[] = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
   currentMonth = this.monthList[this.month];
+
+  message: string = '';
  
+ 
+
 
   constructor() { }
 
   ngOnInit() {   
- 
+    if (this.hours > 21 || this.hours < 9) {
+      this.message = 'С утра будет завтрак';
+    } else if (this.hours > 9 || this.hours < 14) {
+      this.message = 'Не пропустите обед';
+    } else {
+      this.message = 'Время близится к ужину';
+    }
   }
 
 }
