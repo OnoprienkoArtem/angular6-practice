07685246397472baa010fef7aa73eaa1370c70d1
OnoprienkoArtem <o.artem.e@gmail.com>
2018-06-03 00:04:40 +0300

    work with object date add message about how rest

--- task1/src/app/countdown-module/countdown-module.component.ts
+++ task1/src/app/countdown-module/countdown-module.component.ts
@@ -9,7 +9,24 @@ export class CountdownModuleComponent implements OnInit {
 
   constructor() { }
 
+  targetDate = new Date(2018, 6, 10);
+  targetDay = this.targetDate.getDate();
+
+
+  currentDate = new Date();
+  currentDay = this.currentDate.getDate();
+
+  howRest = this.targetDay - this.currentDay;
+  mesagge: string = '';
+
   ngOnInit() {
+    if (this.howRest !== 0) {
+      this.mesagge = 'до целевой даты осталось ' + this.howRest + ' дн';      
+    } else {
+      this.mesagge = 'этот день наступил'; 
+    }
+
+
   }
 
 }
